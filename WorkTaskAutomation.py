5cd8f57d241c95e558bc20366609fcaf17159835
FerasAlhenashil <gobak2020hotmail.com>
2019-08-13 17:21:31 -0700

    inserting into the tracker file stage

--- WorkTaskAutomation.py
+++ WorkTaskAutomation.py
@@ -62,7 +62,6 @@ class WorkTaskAutomation(object):
             print(job[7], 'Error: has no DV Shell due nor PM Prep tasks')
 
     def ws_tracker_schedule(self, schedule, schedule_dates):
-        print('we are in tracker')
         tracker_schedule = []
         for i in range(len(schedule_dates)):
             cell_schedule = []
@@ -71,18 +70,50 @@ class WorkTaskAutomation(object):
                 if (schedule[i][j]) is not None:
                     cell_schedule.append(schedule_str)
             tracker_schedule.append(cell_schedule)
-        print(tracker_schedule[0])
+        #print(tracker_schedule)
         return tracker_schedule
 
-    def ws_append(self, tracker_ws, tracker_schedule, art_works, schedule, titles):
-        last_row = tracker_ws.max_row
-        print(last_row)
+    def ws_append(self, tracker_ws, tracker_schedule, art_works, titles, ws):
+        fini = tracker_ws.max_row
+        while tracker_ws.cell(row=fini, column=1).value is None:
+            fini -= 1
+        print(tracker_ws.cell(row=fini, column=1).value)
+        try:
+            tracker_ws.unmerge_cells(start_row=fini, start_column=1, end_row=fini, end_column=6)
+        except ValueError:
+            print('cells are not merged')
+        tracker_ws.insert_rows(fini)
+        fini += 1
+        #tracker_ws.merge_cells(start_row=fini, start_column=1, end_row=fini, end_column=6)
+        for i in range(len(titles)):
+            self.tracker_header(tracker_ws, titles, fini, art_works, i)
+            nex_project = self.tracker_body(tracker_ws, art_works, fini, tracker_schedule)
 
+    def tracker_header(self, tracker_ws, titles, fini, art_works, i):
+        tracker_ws.cell(row=fini-1, column=1).value = titles[i] + '   ' + '(' + art_works[0][3] + ')'
+        tracker_ws.cell(row=fini-1, column=2).value = 'GK'
+        tracker_ws.cell(row=fini-1, column=3).value = 'DTP'
+        tracker_ws.cell(row=fini-1, column=4).value = 'Status'
+        tracker_ws.cell(row=fini-1, column=5).value = 'Schedule'
+        tracker_ws.cell(row=fini-1, column=6).value = 'Notes'
+
+    def tracker_body(self, tracker_ws, art_works, fini, tracker_schedule):
+        print('tracker_body')
+        i = 0
+        while art_works[0][3] == art_works[i][3]:
+            tracker_ws.insert_rows(fini)
+            fini += 1
+            tracker_ws.cell(row=fini - 1, column=1).value = art_works[i][6] + '   ' + art_works[i][7]
+            tracker_ws.cell(row=fini - 1, column=2).value = art_works[i][1]
+            tracker_ws.cell(row=fini - 1, column=3).value = art_works[i][2]
+            tracker_ws.cell(row=fini - 1, column=5).value = str(tracker_schedule[i])
+            i += 1
+        print('i equals ' + str(i))
 
 
     def API(self):
         os.chdir('C:\\Users\\Feras\\Documents\\Work_Task_Automaiton')
-        wb = openpyxl.load_workbook('Copy of 190510_IN_Rebrand_Schedule_50116 (1).xlsm')
+        wb = openpyxl.load_workbook('50117_Rebrand_Schedule_FR_and_BEDE_190624 - Copy.xlsm')
         ws = wb.active
         tracker_wb = openpyxl.load_workbook('Version2_Melanies_Project_Tracker_MW - Copy.xlsx')
         tracker_ws = tracker_wb.active
@@ -91,19 +122,11 @@ class WorkTaskAutomation(object):
         schedule = self.ws_schedule(art_works)
         schedule_dates = self.ws_schedule_dates(art_works, ws)
         tracker_schedule = self.ws_tracker_schedule(schedule, schedule_dates)
-        self.ws_append(tracker_ws, tracker_schedule, art_works, schedule, titles)
-
-        """for i in range(len(schedule)):
-            print(schedule[i])
-            print(schedule_dates[i])
-            # the last date on schedule is: schedule_dates[i][len(schedule_dates[i]) - 1]
-            #print(schedule_dates[i][len(schedule_dates[i])-1].value)
-        x = len(schedule_dates[0])-1
-        y = 0
-        print(art_works[y][6])
-        print(schedule_dates[y][x].month, '/', schedule_dates[y][x].day, end=' ')
-        print(schedule[y][x])
-        print(len(schedule[y]))"""
+        self.ws_append(tracker_ws, tracker_schedule, art_works, titles, ws)
+        try:
+            tracker_wb.save('row_insert.xlsx')
+        except PermissionError:
+            print('Unable to save to the file. Check if it\'s open')
 
 
 def main():
