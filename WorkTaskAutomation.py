e0103a806f642be8b1e0562edfb4137febb22a54
FerasAlhenashil <gobak2020hotmail.com>
2019-08-19 10:07:20 -0700

    The first fully functional version of the script

--- WorkTaskAutomation.py
+++ WorkTaskAutomation.py
@@ -1,5 +1,7 @@
-import openpyxl
 import os
+import warnings
+from openpyxl import load_workbook
+from openpyxl.styles import NamedStyle, Font, PatternFill, Alignment, Border, Side
 
 
 class WorkTaskAutomation(object):
@@ -10,9 +12,9 @@ class WorkTaskAutomation(object):
         titles = []
         for row in ws:
             for cell in row:
-                if cell.column is 7:
-                    if cell.value is None:
-                        cell = cell.offset(0, 1)
+                if cell.column is 7 and cell.value is None:
+                    cell = cell.offset(0, 1)
+                    if cell.value is not None:
                         titles.append(cell.value)
         return titles
 
@@ -24,14 +26,14 @@ class WorkTaskAutomation(object):
                     art_works.append(row)
         return art_works
 
-    def ws_project_sizes(self, art_works):
-        project_sizes = [0]
+    def ws_project_starting_index(self, art_works):
+        project_starting_index = [0]
         j = 0
         for i in range(len(art_works)):
             if art_works[j][3] != art_works[i][3]:
-                project_sizes.append(i)
+                project_starting_index.append(i)
                 j = i
-        return project_sizes
+        return project_starting_index
 
     def ws_schedule(self, art_works):
         schedule = []
@@ -68,84 +70,146 @@ class WorkTaskAutomation(object):
             start = job.index('PM Prep')
             return start
         except ValueError:
-            print(job[7], 'Error: has no DV Shell due nor PM Prep tasks')
+            try:
+                start = job.index('PM prep')
+                return start
+            except ValueError:
+                print(job[7], 'Error: has no DV Shell due nor PM Prep/prep tasks')
 
     def ws_tracker_schedule(self, schedule, schedule_dates):
         tracker_schedule = []
         for i in range(len(schedule_dates)):
             cell_schedule = []
             for j in range(len(schedule_dates[i])):
-                schedule_str = str(schedule_dates[i][j].month) + "/" + str(schedule_dates[i][j].day) + str(schedule[i][j])
-                if (schedule[i][j]) is not None:
-                    cell_schedule.append(schedule_str)
+                try:
+                    schedule_str = " " + str(schedule_dates[i][j].month) + "/" + str(schedule_dates[i][j].day) + " " + str(
+                        schedule[i][j])
+                    if (schedule[i][j]) is not None:
+                        cell_schedule.append(schedule_str)
+                except AttributeError:
+                    print('An error with schedule dates')
             tracker_schedule.append(cell_schedule)
         return tracker_schedule
+
     def get_fini(self, tracker_ws):
         fini = tracker_ws.max_row
         while tracker_ws.cell(row=fini, column=1).value is None:
             fini -= 1
         return fini
 
-    def ws_append(self, tracker_ws, tracker_schedule, art_works, titles, project_sizes):
+    def ws_append(self, tracker_ws, tracker_schedule, art_works, titles, project_starting_index, tracker_wb):
         fini = self.get_fini(tracker_ws)
         try:
             tracker_ws.unmerge_cells(start_row=fini, start_column=1, end_row=fini, end_column=6)
         except ValueError:
             print('cells are not merged')
-        #tracker_ws.merge_cells(start_row=fini, start_column=1, end_row=fini, end_column=6)
+        thin = Side(border_style="thin", color="000000")
+        top_style = NamedStyle(name="top_style", fill=PatternFill(patternType='solid', fill_type='solid', fgColor=
+            "c5d9f1"), font=Font(name='Calibri', size=16), alignment=Alignment(horizontal="center", vertical="center"),
+                               border=Border(top=thin, left=thin, right=thin, bottom=thin))
+        body1 = NamedStyle(name="body1", font=Font(name="Calibri", size=16), border=Border(top=thin, left=thin, right=
+            thin, bottom=thin), alignment=Alignment(horizontal="left", vertical="top"))
+        try:
+            tracker_wb.add_named_style(top_style)
+            tracker_wb.add_named_style(body1)
+        except ValueError:
+            pass
         for i in range(len(titles)):
+            fini = self.get_fini(tracker_ws)
+            tracker_ws.insert_rows(fini)
+            try:
+                self.tracker_header(tracker_ws, titles, art_works, i, project_starting_index[i])
+                self.tracker_body(tracker_ws, art_works, tracker_schedule, project_starting_index[i])
+            except IndexError:
+                print('There was an issue with the arrays that caused an error')
+
+    def tracker_header(self, tracker_ws, titles, art_works, i, project_starting_index):
+        fini = self.get_fini(tracker_ws)
+        tracker_ws.row_dimensions[fini-1].height = 100
+        cell1 = tracker_ws.cell(row=fini-1, column=1)
+        cell2 = tracker_ws.cell(row=fini-1, column=2)
+        cell3 = tracker_ws.cell(row=fini-1, column=3)
+        cell4 = tracker_ws.cell(row=fini-1, column=4)
+        cell5 = tracker_ws.cell(row=fini-1, column=5)
+        cell6 = tracker_ws.cell(row=fini-1, column=6)
+
+        cell1.value = titles[i] + '   ' + '(' + art_works[project_starting_index][3] + ')'
+        cell2.value = 'GK'
+        cell3.value = 'DTP'
+        cell4.value = 'Status'
+        cell5.value = 'Schedule'
+        cell6.value = 'Notes'
+        cell1.style = 'top_style'
+        cell2.style = 'top_style'
+        cell3.style = 'top_style'
+        cell4.style = 'top_style'
+        cell5.style = 'top_style'
+        cell6.style = 'top_style'
+
+    def tracker_body(self, tracker_ws, art_works, tracker_schedule, project_starting_index):
+        i = project_starting_index
+        fini = self.get_fini(tracker_ws)
+        while art_works[project_starting_index][3] == art_works[i][3]:
             tracker_ws.insert_rows(fini)
             fini = self.get_fini(tracker_ws)
-            self.tracker_header(tracker_ws, titles, fini, art_works, i, project_sizes[i])
-            self.tracker_body(tracker_ws, art_works, fini, tracker_schedule, project_sizes[i])
-
-    def tracker_header(self, tracker_ws, titles, fini, art_works, i, project_sizes):
-        print('tracker_header and i equals: ' + str(i))
-        tracker_ws.cell(row=fini-1, column=1).value = titles[i] + '   ' + '(' + art_works[project_sizes][3] + ')'
-        tracker_ws.cell(row=fini-1, column=2).value = 'GK'
-        tracker_ws.cell(row=fini-1, column=3).value = 'DTP'
-        tracker_ws.cell(row=fini-1, column=4).value = 'Status'
-        tracker_ws.cell(row=fini-1, column=5).value = 'Schedule'
-        tracker_ws.cell(row=fini-1, column=6).value = 'Notes'
-
-    def tracker_body(self, tracker_ws, art_works, fini, tracker_schedule, project_size):
-        print('tracker_body project size is: ' + str(project_size))
-        i = project_size
-        while art_works[project_size][3] == art_works[i][3]:
-            tracker_ws.insert_rows(fini)
-            fini = self.get_fini(tracker_ws)
-            tracker_ws.cell(row=fini - 1, column=1).value = art_works[i][6] + '   ' + art_works[i][7]
-            tracker_ws.cell(row=fini - 1, column=2).value = art_works[i][1]
-            tracker_ws.cell(row=fini - 1, column=3).value = art_works[i][2]
-            tracker_ws.cell(row=fini - 1, column=5).value = str(tracker_schedule[i])
+            tracker_ws.row_dimensions[fini - 1].height = 70
+            cell1 = tracker_ws.cell(row=fini - 1, column=1)
+            cell2 = tracker_ws.cell(row=fini - 1, column=2)
+            cell3 = tracker_ws.cell(row=fini - 1, column=3)
+            cell5 = tracker_ws.cell(row=fini - 1, column=5)
+            cell1.value = art_works[i][6] + '   ' + art_works[i][7]
+            cell2.value = art_works[i][1]
+            cell3.value = art_works[i][2]
+            cell5.value = ','.join(tracker_schedule[i])
+            cell1.style = 'body1'
+            cell2.style = 'body1'
+            cell3.style = 'body1'
+            cell5.style = 'body1'
             i += 1
             if i > len(art_works)-1:
                 break
-        print('i equals ' + str(i))
         return i
 
     def API(self):
         os.chdir('C:\\Users\\Feras\\Documents\\Work_Task_Automaiton')
-        wb = openpyxl.load_workbook('190605_Frazier3_AU_schedule_48041.xlsm')
+        schedule_file = input('Enter the schedule file name: ')
+        schedule_file = schedule_file + '.xlsm'
+        try:
+            wb = load_workbook(schedule_file)
+        except FileNotFoundError:
+            print('Unable to find the schedule file. Make sure it exists at the expected location')
+            quit()
         ws = wb.active
-        tracker_wb = openpyxl.load_workbook('Version2_Melanies_Project_Tracker_MW - Copy2.xlsx')
+        tracker_file = input('Enter the tracker file to load: ')
+        tracker_file = tracker_file + '.xlsx'
+        try:
+            tracker_wb = load_workbook(tracker_file)
+        except FileNotFoundError:
+            print('Unable to find the tracker file. Make sure it exists at the expected location')
+            quit()
         tracker_ws = tracker_wb.active
         art_works = self.ws_art_works(ws)
-        project_sizes = self.ws_project_sizes(art_works)
+        project_starting_index = self.ws_project_starting_index(art_works)
         titles = self.ws_titles(ws)
         schedule = self.ws_schedule(art_works)
         schedule_dates = self.ws_schedule_dates(art_works, ws)
         tracker_schedule = self.ws_tracker_schedule(schedule, schedule_dates)
-        self.ws_append(tracker_ws, tracker_schedule, art_works, titles, project_sizes)
+        self.ws_append(tracker_ws, tracker_schedule, art_works, titles, project_starting_index, tracker_wb)
+        fini = self.get_fini(tracker_ws)
+        tracker_ws.merge_cells(start_row=fini, start_column=1, end_row=fini, end_column=6)
+        tracker_file2 = input('Enter the tracker file to save: ')
+        tracker_file2 = tracker_file2 + '.xlsx'
         try:
-            tracker_wb.save('Version2_Melanies_Project_Tracker_MW - Copy2.xlsx')
+            tracker_wb.save(tracker_file2)
         except PermissionError:
-            print('Unable to save to the file. Check if it\'s open')
+            print('Unable to save to the tracker. Please make sure the file isn\'t open')
 
 
 def main():
     obj = WorkTaskAutomation()
-    obj.API()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        obj.API()
 
 
 if __name__ == '__main__':
